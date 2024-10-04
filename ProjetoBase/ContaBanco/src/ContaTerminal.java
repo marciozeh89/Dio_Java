abb165643be848298df2a5bb9c7ae72731cb0e9e
marciozeh89 <marciozeh@gmail.com>
2024-10-03 23:23:09 -0300

    trabalho conta terminal

--- ProjetoBase/ContaBanco/src/ContaTerminal.java
+++ ProjetoBase/ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Entre com o numero da conta!");
+        int conta = sc.nextInt();
+
+        System.out.println("Entre com a agencia!");
+        String agencia = sc.next();
+        sc.nextLine();
+
+        System.out.println("Entre com o nome do cliente!");
+        String nome = sc.next();
+        //sc.nextLine();
+
+        System.out.println("Entre com o sobrenome do cliente!");
+        String sobrenome = sc.next();
+        //sc.nextLine();
+
+        System.out.println("Entre com o valor do Saldo!");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Ola " + nome + " " + sobrenome + " " 
+        + "obrigado por criar uma conta em nosso banco, sua agencia e " + agencia 
+        + " , conta " + conta + "e seu saldo R$ " + saldo + " ja esta disponivel para saque");
+        sc.close();
     }
 }
